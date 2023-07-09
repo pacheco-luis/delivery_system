0a65b8db34a65ecd7d9733c5632c55ede99b8618
Luis Pacheco <pachecoluis9615@gmail.com>
2023-07-09 15:46:21 +0800

    created user database model and read operation

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
+
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    customer_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    username = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', default='profile_images/user-default.png', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.username)
--- users/views.py
+++ users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'users/home.html')
+
+def userAccount(request):
+    return render(request, 'users/account.html')
