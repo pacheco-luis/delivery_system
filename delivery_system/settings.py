92a31f8e2ad2efc2f8b4c48c61d3509dba528d3a
Luis Pacheco <pachecoluis9615@gmail.com>
2023-08-13 06:11:51 +0800

    added sass to the project using nodejs

--- delivery_system/settings.py
+++ delivery_system/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_URL = '/images/'
+MEDIA_URL = 'images/'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
