ad102ad5106432ecd0f4d0c203f103f612ebd194
Luis Pacheco <pachecoluis9615@gmail.com>
2023-08-28 11:02:56 +0800

    configured reset password for users

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,6 +8,12 @@ urlpatterns = [
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerUser, name='register'),
+
+    # reset password urls
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"), name='password_reset_complete'),
 
     # customer urls
     path('account/', views.account, name='account'),
