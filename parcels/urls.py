91e9dd983075a05274686f3d0bf7963aa295441b
Luis Pacheco <pachecoluis9615@gmail.com>
2023-08-28 10:34:23 +0800

    created parcels list page

--- parcels/urls.py
+++ parcels/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # customer urls
     path('', views.home, name='home'),
+    path('parcels/', views.parcels, name='parcels'),
 ]
 
--- parcels/views.py
+++ parcels/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Parcel
+
 # Create your views here.
 @login_required(login_url='login')
 def home(request):
     return render(request, 'customer/home.html')
+
+@login_required(login_url='login')
+def parcels(request):
+    customer = request.user.customer
+    parcels = Parcel.objects.filter(customer=customer)
+    context = { 'parcels': parcels }
+    return render(request, 'customer/parcels.html', context)
