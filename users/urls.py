cc94dcd5728957b1e51359184d8c93907e638dee
Credit-Car <clara06choc@gmail.com>
2023-09-07 20:27:06 +0800

    included a success_url parameter of class Based View PasswordResetView

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from django.contrib.auth import views as auth_views
 from . import views
@@ -12,7 +12,7 @@ urlpatterns = [
     path('register/', views.registerUser, name='register'),
 
     # reset password urls
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name='reset_password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html", success_url = reverse_lazy('users:password_reset_done')), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"), name='password_reset_complete'),
