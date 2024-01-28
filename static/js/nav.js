197c224b5a4fb4d29f966972fe64ad4f75eec504
samchavita <ianchavarria00@gmail.com>
2024-01-29 02:26:36 +0800

    updated navbar to be fully responsive

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,38 +1,46 @@
 
-function show_menu(){
-  var sidebar = document.getElementById("sidebar");
-  var close_ele = document.getElementById("close_menu");
 
-  if (sidebar) {
-    var currentDisplay = window.getComputedStyle(sidebar).getPropertyValue('display');
-    sidebar.style.display = 'block';
-  }
+window.onresize = function() {
+  var menu_open_btn = document.getElementById("menu");
+  var menu_close_btn = document.getElementById("close_menu");
+  var menu_sidebar = document.getElementById("sidebar");
 
-  if( close_ele ){
-    var menu = document.getElementById("menu");
-    if( menu ){
-      menu.style.display = 'none';
+  if( menu_open_btn && menu_close_btn && menu_sidebar){
+    if( window.innerWidth > 600  ){
+        menu_open_btn.style.display="none";
+        menu_sidebar.style.display = "block";
     }
-
-    close_ele.style.display = 'block';
+    else{
+      menu_open_btn.style.display="block";
+      menu_sidebar.style.display = "none";
+    }
+    menu_close_btn.style.display="none";
   }
+};
+
+
+
+function show_menu(){
+  sidebar_helper("block");
 }
 
 function close_menu(){
+  sidebar_helper("none");
+}
+
+function sidebar_helper( sidebar_display ) {
+
   var sidebar = document.getElementById("sidebar");
-  var menu_btn = document.getElementById("menu");
+  var open_btn = document.getElementById("menu");
+  var close_btn = document.getElementById("close_menu");
+  var body = document.getElementById("base_body");
 
   if (sidebar) {
-    var currentDisplay = window.getComputedStyle(sidebar).getPropertyValue('display');
-    sidebar.style.display = 'none';
+    sidebar.style.display = sidebar_display;
+    close_btn.style.display = sidebar_display;
+
+    open_btn.style.display = ( sidebar_display === "none" ) ? "block": "none";
+    body.style.overflow = ( sidebar_display === "none" ) ? "auto": "hidden";
   }
 
-  if( menu_btn ){
-    var close_menu = document.getElementById("close_menu");
-    if( close_menu ){
-      close_menu.style.display = 'none';
-    }
-
-    menu_btn.style.display = 'block';
-  }
 }
