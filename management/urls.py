76719b06158284f6f123e01ead37705eb43fbd61
sam <ianchavarria00@gmail.com>
2024-04-15 00:01:52 +0800

    added package page to management app

--- management/urls.py
+++ management/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('admin_routes/create/assign/', views.create_assign_routes, name='routes'),
     path('admin_stations/', views.admin_stations, name='admin_stations'),
     path('admin_all_users/', views.admin_all_users, name='admin_all_users'),
+    path('admin_packages/', views.admin_packages, name='admin_packages'),
 ]
